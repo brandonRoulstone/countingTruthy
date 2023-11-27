702589873adfafcc81c7aee2f8d24215012c4e2f
Brandon Roulstone <brandonroulstone71@gmail.com>
2023-11-27 16:20:25 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let newDisplay = document.getElementById("result");
+
+let findTruth = document.getElementById("btnTruth");
+
+
+let arrOfTruth = [true,true, false, true, false, false, true, true, false, false, true];
+
+// function will only find booleans that is equivalent to true
+function countTruthy(arrOfTruth) {
+  let isTruth = 0;
+
+  for (let x of arrOfTruth) {
+
+    if (typeof x !== "boolean") {
+      return "404 error";
+    }
+
+    if (x === true) {
+      isTruth++;
+    }
+
+  }
+  return isTruth;
+}
+
+function res () {
+
+    let truth = countTruthy(arrOfTruth);
+
+    newDisplay.value = (typeof truth === "number") ? truth : truth;
+
+}
+
+
+findTruth.addEventListener("click",res);
